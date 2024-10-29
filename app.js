e4b4bf67ad05c358a3be1be025f42284c226c72d
Oscar Marin <oscarmmarin45@hotmail.com>
2024-10-29 16:36:31 -0600

    a

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 // Catch-all route for handling 404 errors
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-  });
+// app.use((req, res, next) => {
+//     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+//   });
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,11 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../views/index.html'));
 });
+router.get('/views/dashboard.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/dashboard.html'));
+});
+router.get('/views/register.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/register.html'));
+});
 
 module.exports = router;
